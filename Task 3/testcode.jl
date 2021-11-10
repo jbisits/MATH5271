51c06923e6d260d8b29caa10a6305e9ca1e597ff
jbisits <75812103+jbisits@users.noreply.github.com>
2021-11-10 23:58:25 +1100

    More work on task 3

---
+++ Task 3/testcode.jl
@@ -0,0 +1,19 @@
+
+#Alternate linear model fit. GLM package is a little nicer
+x = u1[1:N]
+X = [ones(length(x)) x]
+y = ent_rain_df[1:N, :ent_anom]
+
+
+linfit = X \ y
+
+yhat = @. linfit[1] + linfit[2] * x;
+println("Intercept = "*string(linfit[1])*"\nSlope = "*string(linfit[2]))
+
+x = ent_rain_df[1:N, :rain_48_anom]
+X = [ones(length(x)) x]
+y = ent_rain_df[1:N, :ent_anom]
+
+linfit = X \ y
+
+yhat = @. linfit[1] + linfit[2] * x;
