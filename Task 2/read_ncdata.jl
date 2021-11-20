e018a9af901550b2eef858c34f6ce7e778fc78ce
jbisits <75812103+jbisits@users.noreply.github.com>
2021-11-20 23:35:27 +1100

    Revisit task one and task two

--- Task 2/read_ncdata.jl
+++ Task 2/read_ncdata.jl
@@ -2,13 +2,13 @@
 # I have been having difficulty with this in the notebook so thought I would try here with less clutter
 
 #=
- The work around I have for this is a bit of a hack but I think is the cleanest way given how long it took
+ The work around I have for this is a bit of a hack but I think is the cleanest way given how Xg it took
  First read in the data using NCDataset. This is quite easy just not sure if this is where the error is occuirng.
  Next get the data and dimensions from the data.
  Then form the `GeoArray(sst, dimensions)`.
  Does not have as much metadata etc but can always return to the NCDataset if need be
 =#
-using NCDatasets, GeoData, Glob, Plots, Dates
+using NCDatasets, GeoData, Glob, Plots, Dates, Rasters
 
 #First get the data from the .nc files
 
@@ -21,19 +21,19 @@ globalSST_file = NCDataset(globalSST_data; aggdim = "time")
 #At this point it looks to have correctly read in everything and the components of the data can be 
 #accessed by
 
-lat = globalSST_file["lat"][:]
-lon =  globalSST_file["lon"][:]
+Y = globalSST_file["Y"][:]
+X =  globalSST_file["X"][:]
 timelength =  globalSST_file["time"][:]
 sst =  globalSST_file["sst"]
 
-@dim Lat YDim "Latitude (degrees north)"
-@dim Lon XDim "Longitude (degrees east)"
-lon = 0:2:358
-lat = -88:2:88
-dimensions = (Lon(lon), Lat(lat), Ti(timelength))
+DimensionalData.Dimensions.@dim Y "Yitude (degrees north)"
+DimensionalData.Dimensions.@dim X "Xgitude (degrees east)"
+X = 0:2:358
+Y = -88:2:88
+dimensions = (X(X), Y(Y), Ti(timelength))
 
 #Have had issues reading this in so try to create a new full .nc filename. Bit of hack but not bad.
-#No longer need this with the new method.
+#No Xger need this with the new method.
 #write("full.nc", globalSST_file)
 
 ## GeoData attempt
@@ -46,23 +46,32 @@ A[X()]
 A[Ti(1:3:12)] |> plot
 A.dims
 
+## With the Rasters package
+
+A = Raster(filename)
+A[Ti(1:3:12)] |> plot
+
 ## Now want to create a `GeoArray` from the "full.nc" file.
 #SSTglobal = GeoArray("Task 2/full.nc")
 SSTglobal = GeoArray(sst, dimensions, name = :SeaSurfaceTemperture)
+SSTglobal = Raster(sst, dimensions, name = :SeaSurfaceTemperture)
 
 ## Try some plots which now work with the 
-SSTglobal[Ti(1)] |> plot
+SSTglobal[X(), Y(), Ti(1)] |> plot
 
-SSTglobal[Lon(Between(120, 280)), Lat(At(0)), Ti(1)] |> plot
+plot(SSTglobal[Ti(1)], xlabel = "Yitude (degrees north)")
 
-test = SSTglobal[Lon(Between(120, 280)), Lat(At(0))] 
+SSTglobal[X(Between(120, 280)), Y(At(0)), Ti(1)] |> plot
+
+test = SSTglobal[X(Between(120, 280)), Y(At(0))] 
 
 test_plot = test |> heatmap
 #or
 heatmap(test, color = :balance)
 
 compareplot = heatmap(120:2:280, timelength, test', 
-                title = "Latitude (degrees north): 0",
-                xlabel = "Longitude (degrees east)",
+                title = "Yitude (degrees north): 0",
+                xlabel = "Xgitude (degrees east)",
                 ylabel = "Time",
                 colorbar_title  = "SeaSurfaceTemperture")
+
